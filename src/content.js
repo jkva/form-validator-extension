3a65260f77d293b8d62d76cc459f8556b5a1ddfc
Job van Achterberg <mail@3ode.nl>
2018-03-24 20:49:21 +0100

    Collect input data

--- src/content.js
+++ src/content.js
@@ -1,17 +1,32 @@
-const collectFormData = () => {
-    return { foo : 'bar' };
+const getLabelForNode = node => {
+    const id = node.id;
+    const labelNode = document.querySelector('label[for="' + id + '"]');
+    if (labelNode) {
+        return labelNode.textContent;
+    }
+
+    return null;
+};
+
+const collectAutoCompleteInputs = () => {
+    const inputs = Array.from(document.querySelectorAll('input[autocomplete]'));
+
+    return inputs.map(node => ({
+        field : node.getAttribute('autocomplete'),
+        label : getLabelForNode(node),
+        value : node.value
+    }));
 };
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        console.log(request);
         // Ignore if not from extension
-        if(sender.tab) { return; }
-
+        if (sender.tab) { return; }
 
         if (request.message && request.message === 'POPUP_OPENED') {
-            const formData = collectFormData;
-            sendResponse({ formData });
+            const autoCompleteInputs = collectAutoCompleteInputs();
+            console.debug(autoCompleteInputs);
+            sendResponse(JSON.stringify({ autoCompleteInputs }));
         }
     }
 );
--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,5 @@
 chrome.tabs.query({active: true, currentWindow: true}, tabs => {
     chrome.tabs.sendMessage(tabs[0].id, { message: 'POPUP_OPENED' }, response => {
-        console.debug('response back', response);
+        console.debug('response back', JSON.parse(response));
     });
 });
